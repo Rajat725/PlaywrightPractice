12c23cd090e07377c6cbc03ecbce590d3d885e55
Rajat725 <imrajat108@gmail.com>
2024-04-27 13:23:22 +0530

    Added Clear Text test in Login Form

--- tests/LoginForm.spec.js
+++ tests/LoginForm.spec.js
@@ -1,6 +1,6 @@
 import {test,expect} from '@playwright/test'
 
-test.only('Wrong Login Cred Test',async({page})=>{
+test('Wrong Login Cred Test',async({page})=>{
 await page.goto('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login');
 await page.locator("input[placeholder='Username']").fill('RajatSharma');
 await page.locator("input[placeholder='Password']").fill('Indigo@123');
@@ -10,3 +10,18 @@ const text=await page.locator(".oxd-alert-content--error").allTextContents();
 console.log("Errorr Is :: "+text);
 
 })
+
+test.only('Clear Text Test',async({page})=>{
+    await page.goto('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login');
+    await page.locator("input[placeholder='Username']").fill('RajatSharma');
+    await page.locator("input[placeholder='Password']").fill('Indigo@123');
+
+    await page.locator("input[placeholder='Username']").fill('');
+    await page.locator("input[placeholder='Password']").fill('');
+
+    await page.locator("input[placeholder='Username']").fill('Admin');
+    await page.locator("input[placeholder='Password']").fill('admin123');
+    await page.locator(".orangehrm-login-button").click();
+    
+    })
+
