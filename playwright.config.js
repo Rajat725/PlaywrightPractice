1e50fb421b8dfb5d45b4ec502942809bb3b5f115
Rajat725 <imrajat108@gmail.com>
2024-04-27 12:34:31 +0530

    Change Config File and Launch Browser Test

--- playwright.config.js
+++ playwright.config.js
@@ -13,13 +13,13 @@ const { defineConfig, devices } = require('@playwright/test');
 module.exports = defineConfig({
   testDir: './tests',
   /* Run tests in files in parallel */
-  fullyParallel: true,
+  //fullyParallel: true,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
-  forbidOnly: !!process.env.CI,
+  //forbidOnly: !!process.env.CI,
   /* Retry on CI only */
-  retries: process.env.CI ? 2 : 0,
+  //retries: process.env.CI ? 2 : 0,
   /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 1 : undefined,
+ // workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
@@ -34,7 +34,7 @@ module.exports = defineConfig({
   },
 
   /* Configure projects for major browsers */
-  projects: [
+  /*projects: [
     {
       name: 'chromium',
       use: { ...devices['Desktop Chrome'] },
@@ -48,7 +48,7 @@ module.exports = defineConfig({
     {
       name: 'webkit',
       use: { ...devices['Desktop Safari'] },
-    },
+    },*/
 
     /* Test against mobile viewports. */
     // {
@@ -69,7 +69,7 @@ module.exports = defineConfig({
     //   name: 'Google Chrome',
     //   use: { ...devices['Desktop Chrome'], channel: 'chrome' },
     // },
-  ],
+  //],
 
   /* Run your local dev server before starting the tests */
   // webServer: {
--- tests/LaunchBrowser.spec.js
+++ tests/LaunchBrowser.spec.js
@@ -1,13 +1,14 @@
 import { test, expect } from "@playwright/test";
 
-test("Launch Browser", async ({ browser }) => {
-  const context = browser.newContext();
+test("Launch Browser", async ({ browser}) => {
+ const context = await browser.newContext();
   const page = await context.newPage();
-  await page.goto("https://the-internet.herokuapp.com/");
-  expect(page).toHaveTitle("The Internet");
+  await page.goto("https://omayo.blogspot.com/");
+  await expect(page).toHaveTitle("omayo (QAFox.com)");
+
 });
 
 test("Launch Browser Second Time", async ({ browser, page }) => {
-  await page.goto("https://the-internet.herokuapp.com/");
-  expect(page).toHaveTitle("The Internet");
+  await page.goto("https://omayo.blogspot.com/");
+  await expect(page).toHaveTitle("omayo (QAFox.com)");
 });
