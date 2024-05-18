4fd48a999559d81b75706abb06ed614f0d31a107
Rajat725 <imrajat108@gmail.com>
2024-05-18 10:45:58 +0530

    Added More Functions in Excel UTILS

--- tests/ExcelPractce.js
+++ tests/ExcelPractce.js
@@ -1,19 +1,47 @@
 //const ExcelJS = require('exceljs');
 import ExcelJS from 'exceljs'
 
-async function workSheetLoader(){
+async function getPriceOfProduct(productName,excelPath){
+let result={rowNo:0,colNo:0};
 const workbook = new ExcelJS.Workbook();
-await workbook.xlsx.readFile('download.xlsx');
+await workbook.xlsx.readFile(excelPath);
 const worksheet = workbook.getWorksheet('Sheet1');
 worksheet.eachRow((row,rowNumber)=>{
 
     row.eachCell((cell,colNumber)=>{
 
-        console.log(cell.value);
+        if(cell.value==productName){
+        result.rowNo=rowNumber;
+        result.colNo=colNumber;}
     })
 
 
 })
-
+const cell=worksheet.getCell(result.rowNo,result.colNo+2);
+return cell.value;
 }
-workSheetLoader();
+
+async function resetPriceOfProduct(productName,price,excelPath){
+    let result={rowNo:0,colNo:0};
+    const workbook = new ExcelJS.Workbook();
+    await workbook.xlsx.readFile(excelPath);
+    const worksheet = workbook.getWorksheet('Sheet1');
+    worksheet.eachRow((row,rowNumber)=>{
+    
+        row.eachCell((cell,colNumber)=>{
+    
+            if(cell.value==productName){
+            result.rowNo=rowNumber;
+            result.colNo=colNumber;}
+        })
+    
+    
+    })
+    const cell=worksheet.getCell(result.rowNo,result.colNo+2);
+    cell.value=price;
+    await workbook.xlsx.writeFile(excelPath);
+    }
+
+console.log(await getPriceOfProduct('Apple','download.xlsx'));
+await resetPriceOfProduct('Apple','600','download.xlsx');
+console.log(await getPriceOfProduct('Apple','download.xlsx'));
